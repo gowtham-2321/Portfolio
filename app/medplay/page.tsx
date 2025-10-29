46317bf1bf815c0c842825b43ddf5b6c72a9a913
Gowtham Jegathesan <gowthamjega2005@gmail.com>
2025-10-29 11:55:52 +0530

    Update page.tsx

--- app/medplay/page.tsx
+++ app/medplay/page.tsx
@@ -38,7 +38,7 @@ export default function medplay() {
 
   const stats = [
     { label: "Monthly Active Sessions", value: "6.8K" },
-    { label: "Total songs Listened", value: "417+" },
+    { label: "Total songs Listened", value: "430k+" },
     { label: "Lines of Code", value: "8.6K" },
     { label: "For all users", value: "FREE" },
   ]
